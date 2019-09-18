761f06a406a651e1ee543a8ed82231d49f057a86
Nicco <funkynicco@gmail.com>
2019-09-18 10:57:17 +0200

    Basic drawing of image done

--- ConsoleImage/Program.cs
+++ ConsoleImage/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleImage
 {
@@ -6,7 +14,139 @@ namespace ConsoleImage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(100, 50);
+            Console.SetBufferSize(100, 50);
+
+            var invertColor = false;
+
+            using (var bitmap = (Bitmap)Bitmap.FromFile("image.bmp"))
+            {
+                int windowWidth = 0;
+                int windowHeight = 0;
+
+                while (true)
+                {
+                    if (Console.WindowWidth != windowWidth ||
+                        Console.WindowHeight != windowHeight)
+                    {
+                        Console.Clear();
+                        DrawToWindow(bitmap, Console.WindowWidth, Console.WindowHeight, invertColor);
+                        windowWidth = Console.WindowWidth;
+                        windowHeight = Console.WindowHeight;
+                    }
+
+                    Thread.Sleep(50);
+                }
+            }
         }
+
+        static void DrawToWindow(Bitmap bitmap, int width, int height, bool invertColors)
+        {
+            using (var smallerImage = (Bitmap)bitmap.GetThumbnailImage(width - 1, height - 1, null, IntPtr.Zero))
+            {
+                var lockRect = new Rectangle(0, 0, smallerImage.Width, smallerImage.Height);
+                var bitmapData = smallerImage.LockBits(lockRect, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                try
+                {
+                    for (int y = 0; y < smallerImage.Height; ++y)
+                    {
+                        for (int x = 0; x < smallerImage.Width; ++x)
+                        {
+                            var pixel = Marshal.ReadInt32(bitmapData.Scan0, y * bitmapData.Stride + x * 4);
+                            Console.SetCursorPosition(x, y);
+                            Console.BackgroundColor = MapPixelToConsoleColor(pixel, invertColors);
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                finally
+                {
+                    smallerImage.UnlockBits(bitmapData);
+                }
+            }
+        }
+
+        static int GetPixelComponent(int pixel, PixelComponent component)
+        {
+            switch (component)
+            {
+                case PixelComponent.Red: return (pixel >> 16) & 0xff;
+                case PixelComponent.Green: return (pixel >> 8) & 0xff;
+                case PixelComponent.Blue: return pixel & 0xff;
+            }
+
+            throw new Exception("Invalid component");
+        }
+
+        static ConsoleColor MapPixelToConsoleColor(int pixel, bool invertColors)
+        {
+            var colors = new List<KeyValuePair<int, ConsoleColor>>();
+            colors.Add(new KeyValuePair<int, ConsoleColor>(0xff0000, ConsoleColor.Red));
+            colors.Add(new KeyValuePair<int, ConsoleColor>(0x00ff00, ConsoleColor.Green));
+            colors.Add(new KeyValuePair<int, ConsoleColor>(0x0000ff, ConsoleColor.Blue));
+            colors.Add(new KeyValuePair<int, ConsoleColor>(0xffff00, ConsoleColor.Yellow));
+            colors.Add(new KeyValuePair<int, ConsoleColor>(0xff00ff, ConsoleColor.Magenta));
+            colors.Add(new KeyValuePair<int, ConsoleColor>(0x00ffff, ConsoleColor.Cyan));
+            colors.Add(new KeyValuePair<int, ConsoleColor>(0xffffff, ConsoleColor.White));
+            colors.Add(new KeyValuePair<int, ConsoleColor>(0x000000, ConsoleColor.Black));
+
+            colors.Sort((a, b) =>
+            {
+                var px_r = GetPixelComponent(pixel, PixelComponent.Red);
+                var px_g = GetPixelComponent(pixel, PixelComponent.Green);
+                var px_b = GetPixelComponent(pixel, PixelComponent.Blue);
+
+                var a_r = GetPixelComponent(a.Key, PixelComponent.Red);
+                var a_g = GetPixelComponent(a.Key, PixelComponent.Green);
+                var a_b = GetPixelComponent(a.Key, PixelComponent.Blue);
+
+                var b_r = GetPixelComponent(b.Key, PixelComponent.Red);
+                var b_g = GetPixelComponent(b.Key, PixelComponent.Green);
+                var b_b = GetPixelComponent(b.Key, PixelComponent.Blue);
+
+#if true
+                int a_dist = (int)Math.Sqrt(Math.Pow(px_r - a_r, 2) + Math.Pow(px_g - a_g, 2) + Math.Pow(px_b - a_b, 2));
+                int b_dist = (int)Math.Sqrt(Math.Pow(px_r - b_r, 2) + Math.Pow(px_g - b_g, 2) + Math.Pow(px_b - b_b, 2));
+
+                if (invertColors)
+                {
+                    if (a_dist < b_dist)
+                        return 1;
+
+                    if (a_dist > b_dist)
+                        return -1;
+                }
+                else
+                {
+                    if (a_dist < b_dist)
+                        return -1;
+
+                    if (a_dist > b_dist)
+                        return 1;
+                }
+
+                return 0;
+#else
+                int cmp_r = (a_r - px_r) + (a_b - px_g) + (a_b - px_b);
+                int cmp_g = (a_g - px_g) + (a_b - px_g) + (a_b - px_b);
+                int cmp_b = (a_b - px_b) + (a_b - px_g) + (a_b - px_b);
+
+                return (cmp_r + cmp_g + cmp_b) / 3;
+#endif
+            });
+
+            return colors[0].Value;
+
+            //var red = ((pixel >> 16) & 0xff) / 127;
+            //var green = ((pixel >> 8) & 0xff) / 127;
+            //var blue = (pixel & 0xff) / 127;
+        }
+    }
+
+    enum PixelComponent
+    {
+        Red,
+        Green,
+        Blue
     }
 }
